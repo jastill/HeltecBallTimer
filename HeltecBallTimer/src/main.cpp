51fdb088d41fe819053e94d811bf6cf21372f747
jastill <john@astill.org>
2020-09-02 23:57:23 -0400

    Added reset button and more interrupt code. Disable start IRQ handler once started

--- HeltecBallTimer/src/main.cpp
+++ HeltecBallTimer/src/main.cpp
@@ -5,9 +5,13 @@
 // the OLED used
 U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ 15, /* data=*/ 4, /* reset=*/ 16);
 
-#define DEBUG 1
+#define DEBUG 0
 #define START_PIN 0
 #define FINISH_PIN 22
+
+#define RESET_PIN 18
+
+#define MAX_STR_LEN 32
 
 // Timer for ball drop
 long timer = 0;
@@ -20,6 +24,18 @@ long startTime = 0;
 
 // Flag to show timer running
 int timerRunningFlag = false;
+
+// Interrupt flags
+int finish = false;
+int finishTriggered = 0;
+int start = false;
+int startTriggered = 0;
+int resetFlag = false;
+
+// Forward declaration
+void IRAM_ATTR startTimer();
+void IRAM_ATTR stopTimer();
+void IRAM_ATTR resetTimer();
 
 /**
  * Setup method is called once at the start.
@@ -36,8 +52,11 @@ void setup()
   u8x8.setFont(u8x8_font_chroma48medium8_r);
 
   // Setup the sensors
-  pinMode(START_PIN,INPUT_PULLDOWN);
-  pinMode(FINISH_PIN,INPUT_PULLDOWN);
+  attachInterrupt(digitalPinToInterrupt(FINISH_PIN),stopTimer,FALLING);
+  attachInterrupt(digitalPinToInterrupt(START_PIN),startTimer,FALLING);
+
+  pinMode(RESET_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(RESET_PIN),resetTimer,FALLING);
 
   // Draw the static text on the display
   u8x8.drawString(0,0,"Ball Timer");
@@ -48,33 +67,27 @@ void setup()
 // Main loop. Called repeatedly
 void loop()
 {
-  // Read the sensor values
-  int start = 0x01 ^ digitalRead(START_PIN);
-  int finish = 0x01 ^  digitalRead(FINISH_PIN);
-
   // Only update the display every 200mS
   if (millis() - displayTimer > 200) {
     displayTimer = millis();
   }
 
   // Display the current sensor values
+  // This uses an array of characters
   char debug[32];
-  sprintf(debug,"Start %i",start);
+  sprintf(debug,"Start %i %i",start,startTriggered);
   u8x8.drawString(1,6,debug);
-  sprintf(debug,"Finish %i",finish);
+  sprintf(debug,"Finish %i %i",finish,finishTriggered);
   u8x8.drawString(1,7,debug);
 
-#if DEBUG
-    Serial.print("Start: ");
-    Serial.println(start);
-    Serial.print("Finish: ");
-    Serial.println(finish);
-#endif
-
   // Start the timer running
-  if (start && timerRunningFlag == false) {
+  if (start && timerRunningFlag == false && finishTriggered == 0) {
+    u8x8.clearLine(4);
     timerRunningFlag = true;
     timer = millis();
+
+    // Detach the start interrupt so we ignore the extra messages
+    detachInterrupt(START_PIN);
 
 #if DEBUG
     Serial.println("Started");
@@ -90,12 +103,57 @@ void loop()
 #endif
 
     timerRunningFlag = false;
+    finish = false;
   }
 
   if (timerRunningFlag == true) {
     long runningTime = millis() - timer;
-    char timeString[10];
-    sprintf(timeString,"%ldmS",runningTime);
+    char timeString[MAX_STR_LEN];
+    snprintf(timeString,32,"%ldmS",runningTime);
     u8x8.drawString(1,4,timeString);
   }
+
+  if (resetFlag == true) {
+    #if DEBUG
+    Serial.println("Reset occurred");
+    #endif
+    startTriggered = 0;
+    start = false;
+    finishTriggered = 0;
+    finish = false;
+
+    timerRunningFlag = false;
+    
+    u8x8.clearLine(4);
+    u8x8.clearLine(6);
+    u8x8.clearLine(7);
+    u8x8.drawString71,4,"0mS");
+
+    // Re attach the start interrupt
+    attachInterrupt(digitalPinToInterrupt(START_PIN),startTimer,FALLING);
+    resetFlag = false;
+  }
+}
+
+/**
+ * Interrupt handler for stopping the timer when the ball passes it
+ */
+void IRAM_ATTR stopTimer() {
+  finishTriggered++;
+  finish = true;
+}
+
+/**
+ * Interrupt handler for starting the timer when the ball passes it
+ */
+void IRAM_ATTR startTimer() {
+  startTriggered++;
+  start = true;
+}
+
+/**
+ * Interrupt handler for resetting the timer
+ */
+void IRAM_ATTR resetTimer() {
+  resetFlag = true;
 }
